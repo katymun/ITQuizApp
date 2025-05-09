4bff1fd07a4841fd78eda628d961eaeab46b1bf0
katymun <munkate2003@gmail.com>
2025-05-09 16:30:41 +0300

    first version

--- quiz-app/vite.config.js
+++ quiz-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ITQuizApp/quiz-app/",
+  base: "/ITQuizApp/",
   plugins: [react()],
 })
